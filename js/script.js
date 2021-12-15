0279422d98377da0d7f09e7a686c87d817a5348d
Igor Batko <igor.batko.batko@gmail.com>
2021-12-15 13:49:27 +0100

    Add script: Draw computer move

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-var computerMove, playerMove;
-playerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
